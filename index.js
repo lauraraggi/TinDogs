91ec478fbfcaa1f89d6d717454516dcb747f9358
Laura Raggi <raggiorama@gmail.com>
2022-09-26 16:47:14 +0200

    Remove instructions from file

--- index.js
+++ index.js
@@ -8,18 +8,6 @@ const acceptBtn = document.querySelector(".footer__btn--accept");
 const btnEls = document.querySelectorAll(".footer__btn");
 const stampEl = document.querySelector(".stamp");
 
-/**
- *
- * Click on button accept => Like image appears on top of dog pic
- * Click on button reject => Nope image appears on top of dog pic
- *
- * After clicking on accept or reject, wait for some second before the profile is removed
- * and another dog is displayed
- *
- * hasbeenswiped should be set to true immediately after you click on the buttons
- * hasbeenliked should be set to true after you click on the accept icon
- */
-
 function getNextDog() {
   const nextDogData = dogs.shift();
   return nextDogData ? new Dog(nextDogData) : {};
