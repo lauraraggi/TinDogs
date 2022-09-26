8e266bf13c3ed1f55d8c84d5954bbc93e4be2806
Laura Raggi <raggiorama@gmail.com>
2022-09-26 16:36:34 +0200

    My solution for Scrimba's TinDogs challenge

--- Dog.js
+++ Dog.js
@@ -1,1 +1,25 @@
 // Create the Dog class here
+
+class Dog {
+  constructor(data) {
+    Object.assign(this, data);
+  }
+
+  getDogHtml() {
+    return `
+        <div class="overlay"></div>
+        <img src="${this.avatar}" alt="" class="dog-profile__image" />
+
+        <div class="info">
+          <h1 class="title">${this.name}, ${this.age}</h1>
+          <p class="description">${this.bio}</p>
+        </div>`;
+  }
+
+  setStamp(el) {
+    let stamp = this.hasBeenLiked ? "like-stamp.png" : "nope-stamp.png";
+    el.innerHTML = `<img src="images/${stamp}" alt="" />`;
+  }
+}
+
+export default Dog;
--- index.js
+++ index.js
@@ -1,1 +1,68 @@
-// Remember to import the data and Dog class!
+import { dogs } from "/data.js";
+import Dog from "/Dog.js";
+
+const dogCard = document.querySelector(".dog-profile");
+const rejectBtn = document.querySelector(".footer__btn--reject");
+const acceptBtn = document.querySelector(".footer__btn--accept");
+
+const btnEls = document.querySelectorAll(".footer__btn");
+const stampEl = document.querySelector(".stamp");
+
+/**
+ *
+ * Click on button accept => Like image appears on top of dog pic
+ * Click on button reject => Nope image appears on top of dog pic
+ *
+ * After clicking on accept or reject, wait for some second before the profile is removed
+ * and another dog is displayed
+ *
+ * hasbeenswiped should be set to true immediately after you click on the buttons
+ * hasbeenliked should be set to true after you click on the accept icon
+ */
+
+function getNextDog() {
+  const nextDogData = dogs.shift();
+  return nextDogData ? new Dog(nextDogData) : {};
+}
+
+function render() {
+  stampEl.innerHTML = "";
+  dogCard.innerHTML = dog.getDogHtml();
+}
+
+function swipe() {
+  setTimeout(function () {
+    dog = getNextDog();
+    render();
+  }, 2000);
+}
+
+function likeOrReject(e) {
+  if (!dog.hasBeenSwiped) {
+    dog.hasBeenSwiped = true;
+    dog.hasBeenLiked =
+      e.currentTarget.getAttribute("data-action") === "like"
+        ? true
+        : e.currentTarget.getAttribute("data-action") === "reject"
+        ? false
+        : false;
+    dog.setStamp(stampEl);
+    if (Object.keys(dogs).length === 0) {
+      setTimeout(() => {
+        dogCard.innerHTML = `<h4>Sorry! No more dogs available</h4>`;
+        stampEl.innerHTML = "";
+        Array.from(btnEls).map((btn) => btn.setAttribute("disabled", ""));
+      }, 2000);
+    } else {
+      swipe();
+    }
+  }
+}
+
+let dog = new Dog(dogs.shift());
+
+for (let i = 0; i < btnEls.length; i++) {
+  btnEls[i].addEventListener("click", likeOrReject);
+}
+
+render();
